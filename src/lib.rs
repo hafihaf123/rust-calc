87efaa7167eaba6745966400a2a082c1b6f717e3
hafihaf123 <misobuchta@gmail.com>
2024-09-15 18:11:16 +0200

    improved parenthesis handling, added consecutive operators handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use anyhow::{anyhow, Context, Result};
 
 const OPERATORS: [char; 10] = ['+', '-', '*', 'x', '×', '/', '÷', '^', '(', ')'];
-const PRIORITY_OPERATORS: [char; 6] = ['*', 'x', '×', '/', '÷', '^'];
+const FIRST_PRIORITY_OPERATORS: [char; 1] = ['^'];
+const SECOND_PRIORITY_OPERATORS: [char; 5] = ['*', 'x', '×', '/', '÷'];
 const NON_PRIORITY_OPERATORS: [char; 2] = ['+', '-'];
 
 pub fn calculate(expression: &str) -> Result<f64> {
-    let mut parsed = parse(&handle_parenthesis(expression)?, &OPERATORS)?;
+    let mut parsed = parse(&handle_parenthesis(expression)?)?;
 
-    perform_operations(&mut parsed, &PRIORITY_OPERATORS)?;
+    perform_operations(&mut parsed, &FIRST_PRIORITY_OPERATORS)?;
+    perform_operations(&mut parsed, &SECOND_PRIORITY_OPERATORS)?;
     perform_operations(&mut parsed, &NON_PRIORITY_OPERATORS)?;
 
     let result = if parsed.len() == 1 {
@@ -17,14 +19,14 @@ pub fn calculate(expression: &str) -> Result<f64> {
     Ok(result)
 }
 
-fn parse(expression: &str, operators: &[char]) -> Result<Vec<String>> {
+fn parse(expression: &str) -> Result<Vec<String>> {
     let binding = expression.replace(",", ".");
     let expression_new = binding.trim();
 
     let mut result = Vec::new();
     let mut last = 0;
     for (index, matched) in expression_new
-        .match_indices(operators) {
+        .match_indices(OPERATORS) {
         if last != index {
             result.push(expression_new[last..index].trim().to_string());
         }
@@ -35,6 +37,8 @@ fn parse(expression: &str, operators: &[char]) -> Result<Vec<String>> {
         result.push(expression_new[last..].trim().to_string());
     }
 
+    handle_consecutive_operators(&mut result)?;
+
     Ok(result)
 }
 
@@ -42,6 +46,11 @@ fn perform_operations(vec: &mut Vec<String>, operators: &[char]) -> Result<()> {
     let mut i = 0;
     while i < vec.len() {
         let s = &vec[i];
+
+        if vec.get(i+1).is_none() {
+            i += 1;
+            continue;
+        }
 
         if s.len() == 1 && operators.contains(&s.chars().next().unwrap()) && i > 0 {
             let a = vec[i - 1].parse::<f64>()
@@ -77,10 +86,14 @@ fn calculate_operation(operation: &str, a: f64, b: f64) -> Result<f64> {
 
 fn handle_parenthesis(expression: &str) -> Result<String> {
     if !expression.contains("(") || !expression.contains(")") {
-        if expression.contains("(") || expression.contains("(") {
+        if expression.contains("(") || expression.contains(")") {
             return Err(anyhow!("Parenthesis not matching"));
         }
         return Ok(expression.to_string());
+    }
+
+    if expression.matches("(").count() != expression.matches(")").count() {
+        return Err(anyhow!("Parenthesis not matching"));
     }
 
     let mut result = expression
@@ -90,9 +103,10 @@ fn handle_parenthesis(expression: &str) -> Result<String> {
 
     let mut i = 1;
     while i < result.len() {
-        let mut parsed_substring = parse(&result[i], &OPERATORS)?;
+        let mut parsed_substring = parse(&result[i])?;
 
-        perform_operations(&mut parsed_substring, &PRIORITY_OPERATORS)?;
+        perform_operations(&mut parsed_substring, &FIRST_PRIORITY_OPERATORS)?;
+        perform_operations(&mut parsed_substring, &SECOND_PRIORITY_OPERATORS)?;
         perform_operations(&mut parsed_substring, &NON_PRIORITY_OPERATORS)?;
 
         if parsed_substring.len() == 1 {
@@ -103,6 +117,55 @@ fn handle_parenthesis(expression: &str) -> Result<String> {
     }
 
     Ok(result.into_iter().collect::<String>())
+}
+
+fn handle_consecutive_operators(vec: &mut Vec<String>) -> Result<()> {
+    let mut i = 0;
+    while i < vec.len() {
+        let s = &vec[i];
+
+        if !(s.len() == 1 && OPERATORS.contains(&s.chars().next().unwrap())) {
+            i += 1;
+            continue;
+        }
+
+        if !(vec.get(i+1).is_some() && OPERATORS.contains(&vec[i+1].chars().next().unwrap())) {
+            i += 1;
+            continue;
+        }
+
+        if vec.get(i+2).is_some() && OPERATORS.contains(&vec[i+2].chars().next().unwrap()) {
+            return Err(anyhow!("3 or more consecutive operators are not allowed"));
+        }
+
+        match (&**s, &*vec[i + 1]) {
+            ("-", "+") => {
+                vec[i+2] = format!("-{}", &vec[i+2]);
+                vec.remove(i);
+            },
+            ("+", "+") => {
+                vec.remove(i);
+            },
+            ("-", "-") => {
+                vec.remove(i);
+                vec[i] = "+".to_string();
+            },
+            ("*", "*") => {
+                vec.remove(i);
+                vec[i] = "^".to_string();
+            },
+            (_, "-") => {
+                vec[i+1] = format!("-{}", &vec[i+2]);
+                vec.remove(i+2);
+            },
+            (a, b) => {
+                return Err(anyhow!("These two consecutive operators are not allowed: '{}' and '{}'", a, b));
+            }
+        }
+        i += 1;
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -132,22 +195,42 @@ mod tests {
 
     #[test]
     fn test_parse() -> Result<()> {
-        assert_eq!(parse("1+1", &OPERATORS)?, vec!["1", "+", "1"]);
-        assert_eq!(parse("1-1*5", &OPERATORS)?, vec!["1", "-", "1", "*", "5"]);
-        assert_eq!(parse("1.2x3", &OPERATORS)?, vec!["1.2", "x", "3"]);
-        assert_eq!(parse("1,2×5^2", &OPERATORS)?, vec!["1.2", "×", "5", "^", "2"]);
-        assert_eq!(parse("112/13÷10000000000000000000", &OPERATORS)?, vec!["112", "/", "13", "÷", "10000000000000000000"]);
+        assert_eq!(parse("1+1")?, vec!["1", "+", "1"]);
+        assert_eq!(parse("1-1*5")?, vec!["1", "-", "1", "*", "5"]);
+        assert_eq!(parse("1.2x3")?, vec!["1.2", "x", "3"]);
+        assert_eq!(parse("1,2×5^2")?, vec!["1.2", "×", "5", "^", "2"]);
+        assert_eq!(parse("112/13÷10000000000000000000")?, vec!["112", "/", "13", "÷", "10000000000000000000"]);
 
         Ok(())
     }
 
     #[test]
     fn test_calculate() -> Result<()> {
+        // basic calculations
         assert_eq!(calculate("1+1")?, 2f64);
         assert_eq!(calculate("1-1*5")?, -4f64);
-        assert_eq!(calculate("1.2x3")?, 3.6);
-        assert_eq!(calculate("1,2×5^2")?, 30f64);
-        assert_eq!(calculate("112/16*1000000000000000")?, 7000000000000000f64);
+        // assert_eq!(calculate("1.2x3")?, 3.6);
+        assert_eq!(calculate("6^2/3")?, 12f64);
+
+        // big numbers
+        assert_eq!(calculate("112/16*1000000000000000000")?, 7000000000000000000f64);
+
+        // parenthesis
+        assert_eq!(calculate("2+(1)")?, 3f64);
+        assert_eq!(calculate("2*(-1)")?, -2f64);
+        assert_eq!(calculate("2/(-1)")?, -2f64);
+        assert_eq!(calculate("12,4*(3+2)+5")?, 67f64);
+        assert_eq!(calculate("12,4*(3+2)+5/(6-1)+3")?, 66f64);
+        assert_eq!(calculate("12,4*(2-3)+5")?, -7.4f64);
+        // assert_eq!(calculate("12,4*(2/(3-1)-3)+5")?, -7.4f64);
+
+        // multiple operators
+        assert_eq!(calculate("12,4-+0,4")?, 12f64);
+        assert_eq!(calculate("12,4++0,4")?, 12.8f64);
+        assert_eq!(calculate("12,4--0,4")?, 12.8f64);
+        assert_eq!(calculate("12,4+-0,4")?, 12f64);
+        assert_eq!(calculate("12,4*-0,5")?, -6.2f64);
+        assert_eq!(calculate("12,4/-0,5")?, -24.8f64);
 
         Ok(())
     }
